f3c04e84826a0c1456fe0bd47aeda84a09016fec
titaniume <titaniume@163.com>
2019-05-09 11:00:36 +0800

    测试git

---
+++ src/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @program: algorithm
+ * @description: 测试git
+ * @author: Mr.Huang
+ * @create: 2019-05-09 10:59
+ **/
+public class Test {
+	public static  void  main(String [] args){
+		System.out.println("测试GIT！");
+	}
+}
+
